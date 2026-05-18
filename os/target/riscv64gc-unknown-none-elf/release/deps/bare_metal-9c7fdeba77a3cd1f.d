db7042a1d3822c70d5214eeb6adfb72fd2eb53c0
CyberWorker-Li <2397113697@qq.com>
2026-05-18 11:16:05 +0800

    实验3：批处理操作系统 李东航23301154

---
+++ os/target/riscv64gc-unknown-none-elf/release/deps/bare_metal-9c7fdeba77a3cd1f.d
@@ -0,0 +1,7 @@
+/mnt/os/target/riscv64gc-unknown-none-elf/release/deps/bare_metal-9c7fdeba77a3cd1f.d: /root/.cargo/registry/src/mirrors.ustc.edu.cn-38d0e5eb5da2abae/bare-metal-0.2.5/src/lib.rs
+
+/mnt/os/target/riscv64gc-unknown-none-elf/release/deps/libbare_metal-9c7fdeba77a3cd1f.rlib: /root/.cargo/registry/src/mirrors.ustc.edu.cn-38d0e5eb5da2abae/bare-metal-0.2.5/src/lib.rs
+
+/mnt/os/target/riscv64gc-unknown-none-elf/release/deps/libbare_metal-9c7fdeba77a3cd1f.rmeta: /root/.cargo/registry/src/mirrors.ustc.edu.cn-38d0e5eb5da2abae/bare-metal-0.2.5/src/lib.rs
+
+/root/.cargo/registry/src/mirrors.ustc.edu.cn-38d0e5eb5da2abae/bare-metal-0.2.5/src/lib.rs:
---
+++ os/target/riscv64gc-unknown-none-elf/release/deps/bit_field-4006876bc233634a.d
@@ -0,0 +1,7 @@
+/mnt/os/target/riscv64gc-unknown-none-elf/release/deps/bit_field-4006876bc233634a.d: /root/.cargo/registry/src/mirrors.ustc.edu.cn-38d0e5eb5da2abae/bit_field-0.10.3/src/lib.rs
+
+/mnt/os/target/riscv64gc-unknown-none-elf/release/deps/libbit_field-4006876bc233634a.rlib: /root/.cargo/registry/src/mirrors.ustc.edu.cn-38d0e5eb5da2abae/bit_field-0.10.3/src/lib.rs
+
+/mnt/os/target/riscv64gc-unknown-none-elf/release/deps/libbit_field-4006876bc233634a.rmeta: /root/.cargo/registry/src/mirrors.ustc.edu.cn-38d0e5eb5da2abae/bit_field-0.10.3/src/lib.rs
+
+/root/.cargo/registry/src/mirrors.ustc.edu.cn-38d0e5eb5da2abae/bit_field-0.10.3/src/lib.rs:
